e42a0bf7f59523a86a56c605a199ba9438e537ea
nahomi2004 <nahomicabrera2004@gmail.com>
2025-06-24 08:52:28 -0500

    Models creados, admin funcionando

---
+++ prroyectociudad/ordenamiento/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from ordenamiento.models import *
+
+# Register your models here.
+admin.site.register(Parroquia)
+admin.site.register(Barrio)
---
+++ prroyectociudad/ordenamiento/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Parroquia(models.Model):
+	ubis = (('norte', 'norte'), ('sur', 'sur'), ('este', 'este'), ('oeste', 'oeste'))
+	tipos = (('urbana', 'urbana'), ('rural', 'rural'))
+
+	nombre = models.CharField(max_length=30)
+	ubicacion = models.CharField(max_length=30, choices=ubis)
+	tipo = models.CharField(max_length=30, choices=tipos)
+
+	def __str__(self):
+		return "%s %s %s" % (self.nombre,
+				self.ubicacion,
+				self.tipo)
+
+
+class Barrio(models.Model):
+	nombre = models.CharField(max_length=30)
+	numero_viviendas = models.IntegerField()
+	numero_parques = models.IntegerField()
+	numero_edfre = models.IntegerField()
+	parroquia = models.ForeignKey(Parroquia, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "%s %d %d %d %s" % (self.nombre,
+				self.numero_viviendas,
+				self.numero_parques,
+				self.numero_edfre,
+				self.parroquia)
